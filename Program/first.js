d2f638123a869dfb4147859d41f3be25b4457eac
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-06-02 21:04:30 +0530

    1

--- Program/first.js
+++ Program/first.js
@@ -3,8 +3,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res) =>{
-    res.send("Hello from the express");
+    res.send("Hello Main Home");
 });
+
+app.get("/about", (req,res) =>{
+    res.send("Hello About");
+});
+
 
 app.listen(8000, ()=>{
     console.log("listening the port 8000");
