da96ce2e47973f10ebe2a6b895a5bf5a44a1b68b
PARIMAL BISWAS <biswasparimal01@gmail.com>
2022-06-02 20:35:26 +0530

    Update app.js

--- 1stProgram/app.js
+++ 1stProgram/app.js
@@ -1,1 +1,13 @@
-<div className=""></div>
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req,res) =>{
+    res.send("Hello from the express");
+});
+
+app.listen(8000, ()=>{
+    console.log("listening the port 8000");
+})
+
+
